c12968bd13374f2d78c00e886483727ec62ae8a6
Marzhan <marzhananbia1@gmail.com>
2025-11-20 23:38:16 +0000

    Part A complete

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,105 @@
 // Create a new router
 const express = require("express")
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
+router.get('/login', function (req, res, next) {
+  res.render('login.ejs')
+})
+
+// shared login handler used by both POST /login and POST /loggedin
+function handleLogin(req, res, next) {
+  const identifier = (req.body.identifier || '').trim()
+  const plainPassword = req.body.password || ''
+
+  if (!identifier || !plainPassword) {
+    return res.send('Login failed: missing username/email or password')
+  }
+
+  // find user by username
+  const sql = "SELECT * FROM users WHERE username = ? LIMIT 1"
+  db.query(sql, [identifier], (err, results) => {
+    if (err) return next(err)
+    if (!results || results.length === 0) {
+      return res.send('Login failed: user not found')
+    }
+
+    const user = results[0]
+    const hashed = user.password || user.hashedPassword || user.hashed_password || user.pass
+    if (!hashed) {
+      return res.send('Login failed: no password stored for this user')
+    }
+
+    bcrypt.compare(plainPassword, hashed, (err, match) => {
+      if (err) return next(err)
+      if (match) {
+        const name = user.first || user.username || user.email || 'user'
+        res.send('Login successful. Welcome ' + name + '!')
+      } else {
+        res.send('Login failed: incorrect password')
+      }
+    })
+  })
+}
+
+router.post('/loggedin', handleLogin)
+router.post('/login', handleLogin)
+
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    const saltRounds = 10
+    const plainPassword = req.body.password
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+    
+  // Store hashed password in your database.
+
+    if (err) return next(err)
+    
+        // Compare the password supplied with the password in the database
+    bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+      if (err) {
+        // TODO: Handle error
+      }
+      else if (result == true) {
+        // TODO: Send message
+      }
+      else {
+        // TODO: Send message
+      }
+    })
+
+
+    // saving data in database (store firstname, lastname, email and hashed password)
+    let sqlquery = "INSERT INTO users (username, first, last, email, hashedPassword) VALUES (?,?,?,?,?)"
+    let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword]
+    db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                    return next(err)
+            }
+            result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email
+            result += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
+            res.send(result)
+
+    })
+
+})
 }); 
 
 
+// route to list all users
+router.get('/list', function(req, res, next) {
+  let sqlquery = "SELECT id, first, last, email FROM users"
+  db.query(sqlquery, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.render('userslist.ejs', { users: result })
+  })
+})
 
 // Export the router object so index.js can access it
 module.exports = router
+
